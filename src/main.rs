9e16d6cd380cc4213ba4d0a7b28955ea4b442146
poneding <poneding@gmail.com>
2025-02-19 21:52:21 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,4 @@
-// use termion::terminal_size;
-
-use std::io::stdout;
-
-use termion::{async_stdin, raw::IntoRawMode};
-
 fn main() {
     println!("name: {}", env!("CARGO_PKG_NAME"));
     println!("version: {}", env!("CARGO_PKG_VERSION"));
-    // let (w, h) = terminal_size().unwrap();
-    let (w, h) = crossterm::terminal::size().unwrap();
-    println!("width: {}", w);
-    println!("height: {}", h);
-
-    stdout().lock().into_raw_mode().unwrap();
-
-    let stdin = async_stdin();
 }
