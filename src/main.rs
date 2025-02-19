920f60cb99f7acf03b6445a032040ae3631968b0
poneding <poneding@gmail.com>
2025-02-19 10:18:26 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use termion::terminal_size;
+
 fn main() {
     println!("name: {}", env!("CARGO_PKG_NAME"));
     println!("version: {}", env!("CARGO_PKG_VERSION"));
+    let (w, h) = terminal_size().unwrap();
+    println!("width: {}", w);
+    println!("height: {}", h);
 }
